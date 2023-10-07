91eb2d4df7f966f2adb4f9f9200c0c90e42aeef7
Abdallah <shiliabdallah1234@gmail.com>
2023-10-07 14:15:29 +0200

    some modification

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,36 +1,43 @@
 import React from "react";
 import "./Navbar.css";
 import { Link } from "react-router-dom";
+import logo from "../Assets/Vector.svg";
+import { logDOM } from "@testing-library/react";
 
 const Navbar = () => {
   return (
-    <div className="header">
-      <Link to="/" className="logo">
-        Home
-      </Link>
-      <nav className="navbar">
-        <Link to="/Clothes">Clothes</Link>
-        <Link to="/Login">Login</Link>
-        <Link to="/Register">register</Link>
-      </nav>
-    </div>
-    // <nav className="mainNavbar">
-    //   <div className="Logo-content">
-    //     <Link to="/">Home</Link>
-    //   </div>
-    //   <div className="navElement">
-    //     <ul>
-    //       <li>
-    //         <Link to="/Clothes">Clothes</Link>
-    //       </li>
-    //     </ul>
-    //   </div>
-
-    //   <div className="login-Register">
-    //     <Link to="/Login">Login | </Link>
+    <nav className="header">
+      <div className="logo-vector">
+        <img src={logo} />
+        <Link to="/">Home</Link>
+      </div>
+      <div className="navbar-element">
+        <div className="navbar-control">
+          <Link to="/Clothes">Clothes</Link>
+          <Link to="/Clothes">Clothes</Link>
+          <Link to="/Clothes">Clothes</Link>
+          <Link to="/Clothes">Clothes</Link>
+        </div>
+        <div className="LoginSinup">
+          <Link to="/Login" className="Login-element">
+            Login
+          </Link>
+        </div>
+        {/* <Link to="/Register" className="Register-element">
+          register
+        </Link> */}
+      </div>
+    </nav>
+    // <div className="header">
+    //   <Link to="/" className="logo">
+    //     Home
+    //   </Link>
+    //   <nav className="navbar">
+    //     <Link to="/Clothes">Clothes</Link>
+    //     <Link to="/Login">Login</Link>
     //     <Link to="/Register">register</Link>
-    //   </div>
-    // </nav>
+    //   </nav>
+    // </div>
   );
 };
 
--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -2,11 +2,22 @@ import React from "react";
 import "./Home.css";
 import { Container } from "../../Components/Index";
 import { Link } from "react-router-dom";
+import logo from "../../Components/Assets/beautiful-girl.png";
 
 const Home = () => {
   return (
     <Container>
-      <header class="join-header">
+      <div className="LEUC-container">
+        <p>LET’S EXPLORE UNIQUE CLOTHES.</p>
+        <div className="unique-container"></div>
+        <div className="let's-container"></div>
+      </div>
+      <div className="paragraphe-container"></div>
+      {/* <img src={logo} /> */}
+      <div className="shop">
+        <div className="shopNow-button"></div>
+      </div>
+      {/* <header class="join-header">
         <h1>Home Page</h1>
       </header>
       <main class="join-main">
@@ -22,7 +33,7 @@ const Home = () => {
             </button>
           </Link>
         </form>
-      </main>
+      </main> */}
     </Container>
   );
 };
