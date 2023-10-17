32b33aef6baf83a39eba83d82660c9b131a161c3
Abdallah <shiliabdallah1234@gmail.com>
2023-10-17 13:10:05 +0200

    fix contact us page

--- src/Pages/ContactUs/ContactUs.js
+++ src/Pages/ContactUs/ContactUs.js
@@ -1,5 +1,8 @@
 import React from "react";
 import "./ContactUs.css";
+import facebookLogo from "../../Components/Assets/facebook_logo.svg";
+import instagramLogo from "../../Components/Assets/instagram_logo.svg";
+import twiterLogo from "../../Components/Assets/twiter_logo.svg";
 
 const ContactUs = () => {
   return (
@@ -9,9 +12,13 @@ const ContactUs = () => {
           <h1>JOIN SHOPPING COMMUNITY TO GET MONTHLY PROMO</h1>
           <p>Type your email down below and be young wild generation</p>
           <div className="email-container">
-            <input type="text" required="required" />
-            <span>Add your email here</span>
-            <button type="submit" class="button">
+            <input
+              name="email"
+              type="text"
+              class="search-box"
+              placeholder="&#767676;  Add your email here"
+            />
+            <button name="submit" class="" type="submit">
               Send
             </button>
           </div>
@@ -22,9 +29,32 @@ const ContactUs = () => {
           <div className="leftFooter">
             <h1>FASHION</h1>
             <p>Complete your style with awesome clothes from us.</p>
+            <div className="socialMedia-container">
+              <img src={facebookLogo} />
+              <img src={instagramLogo} />
+              <img src={twiterLogo} />
+            </div>
           </div>
           <div className="rightFooter">
-            <h1>ha</h1>
+            <div className="first-container">
+              <p>Company</p>
+              <p>About</p>
+              <p>Contact Us</p>
+              <p>Support</p>
+              <p>Careers</p>
+            </div>
+            <div className="second-container">
+              <p>Quick Link</p>
+              <p>Share Location</p>
+              <p>Orders Tracking</p>
+              <p>Size Guide</p>
+              <p>FAQs</p>
+            </div>
+            <div className="third-container">
+              <p>Legal</p>
+              <p>Terms & Conditions</p>
+              <p>Privacy Policy</p>
+            </div>
           </div>
         </div>
       </div>
