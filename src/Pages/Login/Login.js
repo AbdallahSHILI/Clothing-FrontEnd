5ea383ca7bfccacc3b905856730397986e070119
Abdallah <shiliabdallah1234@gmail.com>
2023-10-20 13:08:50 +0200

    fix login page

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,40 +1,39 @@
 import React from "react";
 import "./Login.css";
-import { Container } from "../../Components/Index";
+import logo1 from "../../Components/Assets/Loogin-girl.svg";
 
 const Login = () => {
   return (
-    <div className="login-container">
-      <header class="join-header">
-        <h1>Login Now !</h1>
-      </header>
-      <main class="join-main">
-        <form action="chat.html">
-          <div class="form-control-input">
-            <input
-              type="text"
-              name="username"
-              id="username"
-              required="required"
-            />
-            <span className="label">First Name</span>
-          </div>
-
-          <div class="form-control-input">
-            <input
-              type="password"
-              name="password"
-              id="password"
-              required="required"
-            />
-            <span className="label">Mot de pass</span>
-          </div>
-
-          <button type="submit" class="btn">
-            Login
-          </button>
-        </form>
-      </main>
+    <div className="Login-container">
+      <img src={logo1} />
+      <div className="right-container">
+        <div className="login-content">
+          <h1>Login Now !</h1>
+          <form className="Login-form">
+            <div class="form-control-input">
+              <input
+                type="text"
+                name="username"
+                id="username"
+                required="required"
+              />
+              <span className="label">First Name</span>
+            </div>
+            <div class="form-control-input">
+              <input
+                type="password"
+                name="password"
+                id="password"
+                required="required"
+              />
+              <span className="label">Mot de pass</span>
+            </div>
+            <button type="submit" class="btn">
+              Login
+            </button>
+          </form>
+        </div>
+      </div>
     </div>
   );
 };
