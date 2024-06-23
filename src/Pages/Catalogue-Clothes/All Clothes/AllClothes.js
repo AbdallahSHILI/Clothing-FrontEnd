82ad5fd0ff14e10aed1260b363cc63773b6c6e70
Abdallah <shiliabdallah1234@gmail.com>
2024-06-23 14:20:45 +0200

    Add heart and display and delete Icon

--- src/Pages/Catalogue-Clothes/All Clothes/AllClothes.js
+++ src/Pages/Catalogue-Clothes/All Clothes/AllClothes.js
@@ -1,10 +1,13 @@
 import React, { useState, useEffect } from "react";
 import "./AllClothes.css";
 import Axios from "axios";
-import { faHeart } from "@fortawesome/free-solid-svg-icons";
+import Heart from "react-animated-heart";
+import "react-crud-icons/dist/css/react-crud-icons.css";
+import Icon from "react-crud-icons";
 
 const AllClothes = () => {
   const [Clothes, setClothes] = useState([]);
+  const [clickedHearts, setClickedHearts] = useState([]);
   const API = "http://localhost:3001";
 
   // Fetch data when the component mounts
@@ -13,12 +16,26 @@ const AllClothes = () => {
       try {
         const response = await Axios.get(`${API}/Clothing/Clothes/`);
         setClothes(response.data.doc);
+        // Initialize clickedHearts state with false for each clothing item
+        //Create a new array of length 3 (since there are 3 clothing items).
+        // Fill this array with false values, indicating that none of the hearts are clicked initially.
+        //[false , false , false]
+        setClickedHearts(new Array(response.data.doc.length).fill(false));
       } catch (error) {
         console.error("Fetching Clothes Failed", error);
       }
     };
     fetchClothes();
   }, []);
+
+  const handleHeartClick = (index) => {
+    //First, we create a copy of the current clickedHearts array using the spread operator (...)
+    const newClickedHearts = [...clickedHearts]; // newClickedHearts is [false, false, false]
+    //Next, we toggle the state of the heart at the specified index. If the current state is false, it changes to true, and vice versa:
+    newClickedHearts[index] = !newClickedHearts[index]; //if true the turn to false and vice versa
+    //Finally, we update the clickedHearts state with the modified array:
+    setClickedHearts(newClickedHearts);
+  };
 
   return (
     <div className="models_container">
@@ -35,7 +52,24 @@ const AllClothes = () => {
               style={{ maxWidth: "100%", height: "auto" }}
             />
           )}
-          <FontAwesomeIcon icon={faHeart} className="heart_icon" />
+          <Heart
+            isClick={clickedHearts[index]}
+            onClick={() => handleHeartClick(index)}
+          />
+          <Icon
+            name="delete"
+            tooltip="delete"
+            theme="light"
+            size="medium"
+            // onClick={doSomething}
+          />
+          <Icon
+            name="browse"
+            tooltip="browse"
+            theme="light"
+            size="medium"
+            // onClick={doSomething}
+          />
         </div>
       ))}
     </div>
---
+++ src/Pages/Catalogue-Clothes/Delete Clothes/DeleteClothes.js
@@ -0,0 +1,9 @@
+import React from "react";
+import "./DeleteClothes.css";
+
+const DeleteClothes = () => {
+  const API = "http://localhost:3001";
+  return <div></div>;
+};
+
+export default DeleteClothes;
