9411c4f3871cb33de72ceab485a7fef91977d321
Abdallah <shiliabdallah1234@gmail.com>
2024-08-19 05:17:15 +0200

    try to connect 'makeUserAdmin' buttton with backend

--- src/Components/Index.js
+++ src/Components/Index.js
@@ -1,3 +1,5 @@
 export { default as Navbar } from "./Navbar/Navbar";
 export { default as Container } from "./Container/Container";
 export { default as Footer } from "./Footer/Footer";
+export { default as BackButton } from "./Buttons/Back-Button/BackButton";
+export { default as MakeAdminButton } from "./Buttons/Make-Admin-Button/MakeAdminButton";
--- src/Pages/Dashboard/Users-Page/UsersPage.jsx
+++ src/Pages/Dashboard/Users-Page/UsersPage.jsx
@@ -3,38 +3,42 @@ import Icon from "react-crud-icons";
 import { Link } from "react-router-dom";
 import "./UsersPage.css";
 import { useLocation } from "react-router-dom";
+import { BackButton } from "../../../Components/Index";
 
 const UsersPage = () => {
   const location = useLocation();
   const { users, countUsers } = location.state;
 
   return (
-    <div className="users-container">
-      <h1>All Users</h1>
-      <div className="badge-container">{countUsers} Users</div>
-      <ul className="users-list">
-        {users.map((user) => (
-          <li key={user._id} className="user-item">
-            {/* use the class of "user-details" to make name align wih email */}
-            <div className="user-details">
-              <span className="user_FirstLastName">{user.FirstLastName}</span>
-              <span className="user_info">{user.Email}</span>
-            </div>
-            <div className={`user_role ${user.Role.toLowerCase()}`}>
-              {user.Role}
-            </div>
-            <Link to={`/Dashboard/UsersPage/${user._id}`}>
-              <Icon
-                name="arrow-right"
-                tooltip="More Info"
-                theme="light"
-                size="medium"
-              />
-            </Link>
-          </li>
-        ))}
-      </ul>
-    </div>
+    <>
+      <BackButton />
+      <div className="users-container">
+        <h1>All Users</h1>
+        <div className="badge-container">{countUsers} Users</div>
+        <ul className="users-list">
+          {users.map((user) => (
+            <li key={user._id} className="user-item">
+              {/* use the class of "user-details" to make name align wih email */}
+              <div className="user-details">
+                <span className="user_FirstLastName">{user.FirstLastName}</span>
+                <span className="user_info">{user.Email}</span>
+              </div>
+              <div className={`user_role ${user.Role.toLowerCase()}`}>
+                {user.Role}
+              </div>
+              <Link to={`/Dashboard/UsersPage/${user._id}`}>
+                <Icon
+                  name="arrow-right"
+                  tooltip="More Info"
+                  theme="light"
+                  size="medium"
+                />
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </>
   );
 };
 
