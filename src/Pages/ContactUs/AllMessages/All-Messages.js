583f7adee0a4c7bf0080fcfa3696002b83a5f1cf
Abdallah <shiliabdallah1234@gmail.com>
2024-08-29 06:07:29 +0200

    try to fix all message page

--- src/Pages/ContactUs/AllMessages/All-Messages.js
+++ src/Pages/ContactUs/AllMessages/All-Messages.js
@@ -4,12 +4,15 @@ import Axios from "axios";
 import Cookies from "js-cookie";
 import Icon from "react-crud-icons";
 import { BackButton } from "../../../Components/Index";
+// import ManPic from "../../../../Components/Assets/man.png";
+// import WomenPic from "../../../../Components/Assets/woman.png";
 import CountContainer from "../../Catalogue-Clothes/CountContainer/CountContainer";
 // 1- Import the context to use it for count messages
 import { MessageContext } from "../../../useContext/messageContext";
 
 const AllMessages = () => {
   const [messages, setMessages] = useState([]);
+  const [selectedMessage, setSelectedMessage] = useState(null);
   // 2- Destructure setMessageCount from context
   const { setMessageCount } = useContext(MessageContext);
   const API = "http://localhost:3001";
@@ -36,33 +39,60 @@ const AllMessages = () => {
     fetchMessages();
   }, [setMessageCount]);
 
+  const handleRowClick = (message) => {
+    setSelectedMessage(message._id);
+  };
+
   return (
     <>
       <BackButton />
       <div className="messages-container">
         <h1>All Messages</h1>
         <CountContainer>{messages.length} Messages</CountContainer>
-        <ul className="messages-list">
-          {messages.map((message) => (
-            <li key={message._id} className="message-item">
-              {/* use the class of "user-details" to make name align wih email */}
-              <div className="message-details">
-                <span className="message_FirstLastName">
-                  {message.FirstLastName}
-                </span>
-                <span className="message">{message.Email}</span>
-              </div>
-              {/* <Link to={`/Dashboard/UsersPage/${message._id}`}> */}
-              <Icon
-                name="arrow-right"
-                tooltip="More Info"
-                theme="light"
-                size="medium"
-              />
-              {/* </Link> */}
-            </li>
-          ))}
-        </ul>
+
+        {/* Search and Filter Options */}
+        <div className="search-filter-container">
+          <input type="text" placeholder="Search" className="search-input" />
+          <Icon name="search" size="medium" />
+        </div>
+
+        {/* Messages Table */}
+        <table className="messages-table">
+          <thead>
+            <tr>
+              <th></th>
+              <th>
+                User Name <Icon name="sort" size="small" />
+              </th>
+              <th>
+                Message Type <Icon name="sort" size="small" />
+              </th>
+              <th>
+                Time <Icon name="sort" size="small" />
+              </th>
+              <th>
+                Message <Icon name="sort" size="small" />
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {messages.map((message, index) => (
+              <tr
+                key={message._id}
+                className={
+                  selectedMessage === message._id ? "highlighted-row" : ""
+                }
+                onClick={() => handleRowClick(message)}
+              >
+                <td>{index + 1}</td>
+                <td>{message.FirstLastName}</td>
+                <td>{message.WhatIsAbout}</td>
+                <td>{new Date(message.Date).toLocaleDateString("fr-FR")}</td>
+                <td>{message.Message}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </div>
     </>
   );
