ac73010fae7807752e7896935d8195793ef53bf5
Abdallah <shiliabdallah1234@gmail.com>
2024-09-25 13:43:56 +0200

    cancel offer work successfully , also pic of clothes display successfully in offer card

--- src/Components/PopUp/DeleteOffer/deleteOffer.jsx
+++ src/Components/PopUp/DeleteOffer/deleteOffer.jsx
@@ -1,18 +1,18 @@
 import React, { useState } from "react";
 import axios from "axios";
-import "./DeleteClothes.css";
+import "./deleteOffer.css";
 import Cookies from "js-cookie";
 
-//onDeleteSuccess to automatically the clothes deleted without refreshing the page
-const Modal = ({ isOpen, onClose, clothesId, onDeleteSuccess }) => {
+//onDeleteSuccess to automatically the offer deleted without refreshing the page
+const DeleteOfferModal = ({ isOpen, onClose, offerId, onDeleteSuccess }) => {
   const [success, setSuccess] = useState(false);
   const API = "http://localhost:3001";
-  
+
   const handleDelete = async (e) => {
     e.preventDefault();
     try {
       const token = Cookies.get("access-token");
-      await axios.delete(`${API}/Clothing/Clothes/${clothesId}`, {
+      await axios.delete(`${API}/Clothing/Clothes/DeleteOffer/${offerId}`, {
         headers: {
           authorization: `Bearer ${token}`,
         },
@@ -21,10 +21,10 @@ const Modal = ({ isOpen, onClose, clothesId, onDeleteSuccess }) => {
       setSuccess(true); // Show success message
       setTimeout(() => {
         onClose(); // Close the modal
-        onDeleteSuccess(clothesId); // Notify the parent component about successful deletion
+        onDeleteSuccess(offerId); // Notify the parent component about successful deletion
       }, 1500);
     } catch (error) {
-      console.error("Deleting Clothes Failed", error);
+      console.error("Deleting Offer Failed", error);
     }
   };
 
@@ -41,18 +41,20 @@ const Modal = ({ isOpen, onClose, clothesId, onDeleteSuccess }) => {
         <button className="close-button" onClick={onClose}>
           &times;
         </button>
-        <h2>Delete Clothes</h2>
+        <h2>Delete Offer</h2>
         {success ? (
-          <p>Clothes deleted successfully!</p>
+          <p>Offer deleted successfully!</p>
         ) : (
           <div className="modal-card">
             <p>Are you sure you want to delete this item?</p>
-            <button className="btn delete-clothes" onClick={handleDelete}>
-              Delete
-            </button>
-            <button className="btn cancel-delete" onClick={handleClose}>
-              Cancel
-            </button>
+            <div className="btn-container">
+              <button className="btn delete-clothes" onClick={handleDelete}>
+                Delete
+              </button>
+              <button className="btn cancel-delete" onClick={handleClose}>
+                Cancel
+              </button>
+            </div>
           </div>
         )}
       </div>
@@ -60,4 +62,4 @@ const Modal = ({ isOpen, onClose, clothesId, onDeleteSuccess }) => {
   );
 };
 
-export default Modal;
+export default DeleteOfferModal;
